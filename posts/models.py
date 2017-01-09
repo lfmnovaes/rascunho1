03ef879c4c8a7e24c337717600c6496130234aef
lfmnovaes <lfmnovaes@gmail.com>
2017-01-09 20:20:41 -0200

    learning slug
    lesson 29 done

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,29 @@
 from __future__ import unicode_literals
 
-from django.db import models
 from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+
+from django.utils.text import slugify
 
 # Create your models here.
 # MVC MODEL VIEW CONTROLLER
 
+def upload_location(instance, filename):
+    # filebase, extension = filename.split(".")
+    # return "%s/%s.%s" %(instance.id, intance.id, extension)
+    return "%s/%s" %(instance.slug, filename)
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to=upload_location,
+        null=True,
+        blank=True,
+        height_field="height_field",
+        width_field="width_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -19,4 +35,32 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("posts:detail", kwargs={"id": self.id})
+        return reverse("posts:detail", kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"] #if two objects are created at the same time, order by updated
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+    # slug = slugify(instance.title)
+    # exists = Post.objects.filter(slug=slug).exists()
+    # if exists:
+    #     slug = "%s-%s" %(slug, instance.id)
+    # instance.slug = slug
+
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -6,7 +7,7 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -17,8 +18,8 @@ def post_create(request):
     }
     return render(request, "post_form.html", context)
 
-def post_detail(request, id=None): #retrieve
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug=None): #retrieve
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         "title": instance.title,
         "instance": instance,
@@ -26,16 +27,30 @@ def post_detail(request, id=None): #retrieve
     return render(request, "post_detail.html", context)
 
 def post_list(request): #list items
-    queryset = Post.objects.all()
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         "object_list": queryset,
-        "title": "List"
+        "title": "List",
+        "page_request_var": page_request_var,
     }
     return render(request, "post_list.html", context)
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -49,8 +64,8 @@ def post_update(request, id=None):
     }
     return render(request, "post_form.html", context)
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully deleted")
     return redirect("posts:list")
